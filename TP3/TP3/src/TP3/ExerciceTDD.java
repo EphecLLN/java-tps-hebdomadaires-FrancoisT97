9cb8aa340d4271aab24a93e99c50f5c85dd1b2c8
FrancoisT97 <56149458+FrancoisT97@users.noreply.github.com>
2019-12-31 13:32:43 +0100

    Add files via upload

---
+++ TP3/TP3/src/TP3/ExerciceTDD.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package TP3;
+
+/**
+ * @author François
+ *
+ */
+public class ExerciceTDD {
+    
+    //METHODES
+    /**
+     * Détermine si un nombre est pair ou non
+     * @param n 
+     * @return true si n est pair ou false s'il est impair
+     */
+    public static boolean estPair(int n) {
+	if (n % 2 == 0) {
+	    return true;
+	} else {
+	    return false;
+	}
+    }
+    
+    /**
+     * Calcule le perimetre carré sur base d'un coté de longueur n
+     * @param n longueur d'un coté
+     * @return le perimetre carré
+     */
+    public double perimetreCarre(int n) {
+	return 0;
+    }
+
+    /**
+     * Analyse une String pour déterminer si elle contient un caractère
+     * @param str String à analyser
+     * @return true si le caractère c se trouve dans la string str
+     */
+    public boolean contientCaractere(char c, String str) {
+	return false;
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
